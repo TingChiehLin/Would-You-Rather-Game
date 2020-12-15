59e735e43da5ad2a670215b373752df98e85e530
Jay Lin <cooloojayoo@gmail.com>
2020-12-16 09:51:46 +1100

    weird space - button

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,8 +3,10 @@ import './User.scss';
 
 function User(props) {
     const {name, imageUrl} = props;
+
+    
     return(
-        <div className="user-profile">
+        <div className="user-profile" onClick={()=> {console.log("ss")}}>
             <img className="user-profile-image" src={imageUrl} alt={`${name}`}/>
             <div className="user-profile-name">{name}</div>
         </div>
