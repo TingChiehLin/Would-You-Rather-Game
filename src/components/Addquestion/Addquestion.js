30b32b122a719ef4bc5000f55352e76f272ec01b
Jay Lin <cooloojayoo@gmail.com>
2020-12-27 22:11:46 +1100

    size

--- src/components/Addquestion/Addquestion.js
+++ src/components/Addquestion/Addquestion.js
@@ -8,11 +8,13 @@ function Addquestion() {
     return (
         <div className="askquestion-container">
             <div className="askquestion-container-title">Create a new question</div>
-            <div className="askquestion-container-subtitle">Would you rather?</div>
-            <input type="text" className="askquestion-container-input"/>
-            <span className="askquestion-container-or">OR</span>
-            <input type="text" className="askquestion-container-input"/>
-            <button className="askquestion-container-btn">Sumbit</button>
+            <div className="askquestion-container-content">
+                <div className="askquestion-container-subtitle">Would you rather?</div>
+                <input type="text" className="askquestion-container-input"/>
+                <span className="askquestion-container-or">OR</span>
+                <input type="text" className="askquestion-container-input"/>
+                <button className="askquestion-container-btn">Sumbit</button>
+            </div>
         </div>
     )
 }
